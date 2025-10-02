649f8dd42534cf1cc0189d8edadb625685e26f95
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-10-02 07:41:14 +0300

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const jwt = ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 const auth = require('../middleware/auth'); // تأكد من أن مسار هذا الملف صحيح
